23dfa8fbd5dd82638bae82b3a9ea99ea3c1cf142
MUKILTEO\1722453 <davewilson260@gmail.com>
2025-12-05 12:23:56 -0800

    added graphic settings working on resolution settings

--- Assets/David/Script/QualityChange.cs
+++ Assets/David/Script/QualityChange.cs
@@ -2,41 +2,8 @@ using UnityEngine;
 using TMPro;
 public class QualityChange : MonoBehaviour
 {
- 
-
-    public void handleInputData(int val)
+    public void SetQuality(int qualityIndex)
     {
-        if (val == 2)
-        {
-            low();
-        }
-        else if (val == 1)
-        {
-            medium();
-        }
-        else if (val == 0)
-        {
-            high();
-        }
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
-         
-
-    public void low()
-    {
-        QualitySettings.SetQualityLevel(2, true);
-        Debug.Log("Quality set to  Low");
-    }
-
-    public void medium()
-    {
-        QualitySettings.SetQualityLevel(1, true);
-        Debug.Log("Quality set to medium");
-    }
-
-    public void high()
-    {
-        QualitySettings.SetQualityLevel(0, true);
-        Debug.Log("Quality set to high");
-    }
-
 }
